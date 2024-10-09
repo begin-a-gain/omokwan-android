dcd1bb4f726a94660199af011809c0f719a75409
junyoungleee <jun991203@naver.com>
2024-10-09 21:50:58 +0900

    [feature] add api module with build config and api routes

---
+++ data/src/main/java/com/begin_a_gain/data/constant/ApiRoute.kt
@@ -0,0 +1,8 @@
+package com.begin_a_gain.data.constant
+
+object ApiRoute {
+    fun kakaoLogin() = "/login/oauth/kakao"
+    fun tokenRefresh() = "/token/refresh"
+    fun nickname() = "/nickname"
+    fun nicknameValidation() = "/validations/nickname"
+}
--- data/src/main/java/com/begin_a_gain/data/di/ApiModule.kt
+++ data/src/main/java/com/begin_a_gain/data/di/ApiModule.kt
@@ -1,6 +1,11 @@
 package com.begin_a_gain.data.di
 
 import android.util.Log
+import com.begin_a_gain.data.constant.NETWORK_TIME_OUT
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.DefaultRequest
@@ -10,48 +15,61 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.observer.ResponseObserver
+import io.ktor.client.request.accept
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import javax.inject.Named
+import javax.inject.Singleton
 
-const val NETWORK_TIME_OUT = 10_000L
 
-val httpClient = HttpClient(CIO) {
-    install(ContentNegotiation) {
-        json(
-            Json {
-                prettyPrint = true
-                isLenient = true
-                ignoreUnknownKeys = true
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(
+        @Named("BASE_URL") baseUrl: String
+    ): HttpClient {
+        return HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        prettyPrint = true
+                        isLenient = true
+                        ignoreUnknownKeys = true
+                    }
+                )
             }
-        )
-    }
 
-    install(HttpTimeout) {
-        requestTimeoutMillis = NETWORK_TIME_OUT
-        connectTimeoutMillis = NETWORK_TIME_OUT
-    }
+            install(HttpTimeout) {
+                requestTimeoutMillis = NETWORK_TIME_OUT
+                connectTimeoutMillis = NETWORK_TIME_OUT
+            }
 
-    install(Logging) {
-        level = LogLevel.ALL
-        logger = object : Logger {
-            override fun log(message: String) {
-                Log.d("Network", message)
+            install(Logging) {
+                level = LogLevel.ALL
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.d("Network", message)
+                    }
+                }
+            }
+
+            install(ResponseObserver) {
+                onResponse { response ->
+                    Log.d("HTTP status:", "${response.status.value}")
+                }
+            }
+
+            install(DefaultRequest) {
+                url(baseUrl)
+                accept(ContentType.Application.Json)
+                header(HttpHeaders.ContentType, ContentType.Application.Json)
             }
         }
     }
-
-    install(ResponseObserver) {
-        onResponse { resposne ->
-            Log.d("HTTP status:", "${resposne.status.value}")
-        }
-    }
-
-    install(DefaultRequest) {
-        url("")
-        header(HttpHeaders.ContentType, ContentType.Application.Json)
-    }
-
 }
