0e4f61db195e78845762c092202129c228e42c17
junyoungleee <jun991203@naver.com>
2024-10-12 17:33:40 +0900

    [feature] update OText with color

--- library/src/main/java/com/begin_a_gain/library/design/text/Text.kt
+++ library/src/main/java/com/begin_a_gain/library/design/text/Text.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import com.begin_a_gain.library.design.theme.ColorToken
+import com.begin_a_gain.library.design.theme.ColorToken.Companion.color
 import com.begin_a_gain.library.design.theme.OTextStyle
 import com.begin_a_gain.library.design.theme.suitFontFamily
 
@@ -21,12 +23,14 @@ fun OText(
     text: String,
     style: OTextStyle,
     modifier: Modifier = Modifier,
+    color: ColorToken = ColorToken.text_01,
 ) {
     var lineCount by rememberSaveable { mutableIntStateOf(1) }
 
     Text(
         modifier = modifier,
         text = text,
+        color = color.color(),
         fontFamily = suitFontFamily,
         fontSize = style.typography.fontSize,
         fontWeight = style.typography.fontWeight,
@@ -46,7 +50,9 @@ fun OText(
 @Composable
 private fun OTextPreview() {
     Column(
-        modifier = Modifier.background(Color.White).fillMaxSize()
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize()
     ) {
         OText(text = LoremIpsum().values.first().take(10), style = OTextStyle.Display)
         OText(text = LoremIpsum().values.first().take(10), style = OTextStyle.Headline)
