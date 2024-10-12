58ce8169508d3bce3424c27f25d38fe20c7961b6
junyoungleee <jun991203@naver.com>
2024-10-12 17:32:00 +0900

    [feature] add ColorToken

--- library/src/main/java/com/begin_a_gain/library/design/theme/Color.kt
+++ library/src/main/java/com/begin_a_gain/library/design/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.begin_a_gain.library.design.theme
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 object AppColors {
@@ -25,3 +26,40 @@ object AppColors {
     val Red = Color(0xFF_FF4321)
 }
 
+enum class ColorToken(val lightColor: Color, val darkColor: Color? = null) {
+    ui_backround(AppColors.White),
+    ui_backround_modal(AppColors.Black.copy(alpha = 0.2f)),
+    ui_01(AppColors.Gray50),
+    ui_02(AppColors.Gray100),
+    ui_03(AppColors.Gray200),
+    ui_disable_01(AppColors.Gray200),
+    ui_disable_02(AppColors.Gray400),
+    ui_primary(AppColors.Primary),
+    ui_alert(AppColors.Red),
+
+    stroke_01(AppColors.Gray200),
+    stroke_02(AppColors.Gray300),
+    stroke_03(AppColors.Gray600),
+    stroke_focus(AppColors.Gray900),
+    stroke_disable(AppColors.Gray400),
+    stroke_on_01(AppColors.White),
+    stroke_on_disable(AppColors.Gray600),
+    stroke_primary(AppColors.Primary),
+    stroke_primary_op40(AppColors.Primary.copy(alpha = 0.4f)),
+    stroke_alert(AppColors.Red),
+
+    text_01(AppColors.Gray900),
+    text_02(AppColors.Gray600),
+    text_disable(AppColors.Gray400),
+    text_on_01(AppColors.White),
+    text_on_disable(AppColors.Gray600),
+    text_primary(AppColors.Primary),
+    text_alert(AppColors.Red);
+
+    companion object {
+        @Composable
+        fun ColorToken.color(): Color {
+            return if (isDarkMode()) this.darkColor?: this.lightColor else this.lightColor
+        }
+    }
+}
