b9d230d60f3fd138623bf3a1d3570a54e0b3f1ee
junyoungleee <jun991203@naver.com>
2024-10-29 23:34:25 +0900

    [feat] add screen common component

--- library/src/main/java/com/begin_a_gain/library/design/util/Screen.kt
+++ library/src/main/java/com/begin_a_gain/library/design/util/Screen.kt
@@ -1,11 +1,14 @@
 package com.begin_a_gain.library.design.util
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.begin_a_gain.library.design.component.image.OImageRes
+import com.begin_a_gain.library.design.component.navigation.OTopBar
 import com.begin_a_gain.library.design.theme.ColorToken
 import com.begin_a_gain.library.design.theme.ColorToken.Companion.color
 
@@ -16,3 +19,25 @@ fun Modifier.initScreen(usePadding: Boolean = true) = this
     .fillMaxSize()
     .background(ColorToken.UI_BG.color())
     .padding(horizontal = if (usePadding) ScreenHorizontalPadding else 0.dp)
+
+@Composable
+fun OScreen(
+    modifier: Modifier = Modifier,
+    title: String? = null,
+    showBackButton: Boolean = true,
+    onClickBackButton: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .initScreen()
+            .then(modifier)
+    ) {
+        OTopBar(
+            title = title?: "",
+            startIcon = if (showBackButton) OImageRes.ArrowLeft else null,
+            onClickStart = onClickBackButton
+        )
+        content()
+    }
+}
