f88a702209486786f490b5eab09943dbba5e939f
junyoungleee <jun991203@naver.com>
2024-11-03 13:08:52 +0900

    [feat] update SignInScreen with lifecycle lib

--- feature/src/main/java/com/begin_a_gain/feature/sign_in/SignInScreen.kt
+++ feature/src/main/java/com/begin_a_gain/feature/sign_in/SignInScreen.kt
@@ -1,7 +1,6 @@
 package com.begin_a_gain.feature.sign_in
 
 import android.content.Context
-import android.graphics.drawable.shapes.Shape
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,32 +8,26 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.begin_a_gain.library.design.component.button.OTextButton
 import com.begin_a_gain.library.design.component.text.OText
 import com.begin_a_gain.library.design.theme.AppColors
 import com.begin_a_gain.library.design.theme.ColorToken
 import com.begin_a_gain.library.design.theme.OTextStyle
 import com.begin_a_gain.library.design.util.initScreen
-import com.begin_a_gain.library.design.util.noRippleClickable
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
@@ -45,7 +38,7 @@ import com.kakao.sdk.user.UserApiClient
 fun SignInScreen(
     viewModel: SignInViewModel = hiltViewModel()
 ) {
-    val state by viewModel.container.stateFlow.collectAsState()
+    val state by viewModel.container.stateFlow.collectAsStateWithLifecycle()
     val context = LocalContext.current
 
     val kakaoSignInCallBack: (OAuthToken?, Throwable?) -> Unit = { token, error ->
