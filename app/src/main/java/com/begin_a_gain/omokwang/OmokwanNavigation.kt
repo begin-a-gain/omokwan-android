ff86f00d1f454d3caf33dce6dae6c886bffd3346
junyoungleee <jun991203@naver.com>
2024-11-11 22:54:29 +0900

    [feature] add navigation

--- app/src/main/java/com/begin_a_gain/omokwang/OmokwanNavigation.kt
+++ app/src/main/java/com/begin_a_gain/omokwang/OmokwanNavigation.kt
@@ -1,10 +1,13 @@
 package com.begin_a_gain.omokwang
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import com.begin_a_gain.feature.sign_in.SignInScreen
+import com.begin_a_gain.feature.sign_up.SignUpDoneScreen
 import com.begin_a_gain.feature.sign_up.SignUpScreen
 import kotlinx.serialization.Serializable
 
@@ -13,7 +16,12 @@ import kotlinx.serialization.Serializable
 @Serializable object SignUpDone
 
 @Serializable object Main
+@Serializable object OmokList
 @Serializable object MyPage
+
+@Serializable object MakeMatch
+@Serializable object JoinMatch
+@Serializable object Alarm
 
 @Composable
 fun OmokwanNavigation(
@@ -21,7 +29,6 @@ fun OmokwanNavigation(
     startDestination: Any
 ) {
     NavHost(navController = navController, startDestination = startDestination) {
-
         composable<SignIn> {
             SignInScreen(
                 navigateToSignUp = { navController.navigate(SignUp) },
@@ -35,5 +42,22 @@ fun OmokwanNavigation(
                 popBack = { navController.popBackStack() }
             )
         }
+
+        composable<SignUpDone> {
+            SignUpDoneScreen(
+                navigateToMain = { navController.navigate(Main) }
+            )
+        }
+
+        this.bottomNavigation()
+    }
+}
+
+fun NavGraphBuilder.bottomNavigation(
+) {
+    this.navigation<Main>(startDestination = OmokList) {
+        composable<OmokList> { }
+
+        composable<MyPage> {  }
     }
 }
