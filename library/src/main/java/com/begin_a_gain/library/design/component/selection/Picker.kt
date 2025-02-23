4ad891221301a9770f2c03a01deac077419d0ceb
junyoungleee <jun991203@naver.com>
2025-02-23 20:50:03 +0900

    [feature] add picker bottom sheet and update picker

--- library/src/main/java/com/begin_a_gain/library/design/component/selection/Picker.kt
+++ library/src/main/java/com/begin_a_gain/library/design/component/selection/Picker.kt
@@ -1,32 +1,19 @@
 package com.begin_a_gain.library.design.component.selection
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.CompositingStrategy
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.begin_a_gain.library.design.component.text.OText
 import com.begin_a_gain.library.design.theme.AppColors
@@ -37,17 +24,13 @@ val pickerItemHeight = 40.dp
 
 @Composable
 fun OPicker(
-    options: List<String>,
-    selectedIndex: Int,
-    modifier: Modifier = Modifier,
-    onSelect: (Int) -> Unit
+    items: List<String>,
+    lazyListState: LazyListState,
+    modifier: Modifier = Modifier
 ) {
-    val pickerState = rememberLazyListState(0)
-    val selectedIndex by remember {
-        derivedStateOf { pickerState.firstVisibleItemIndex }
-    }
-
-    Column {
+    Column(
+        modifier = modifier
+    ) {
         Box(
             contentAlignment = Alignment.Center
         ) {
@@ -72,45 +55,24 @@ fun OPicker(
             )
 
             LazyColumn(
-                state = pickerState,
-                modifier = modifier
+                state = lazyListState,
+                modifier = Modifier
                     .fillMaxWidth()
                     .fadingEdge(),
                 contentPadding = PaddingValues(0.dp, pickerItemHeight * 2),
-                flingBehavior = rememberSnapFlingBehavior(lazyListState = pickerState)
+                flingBehavior = rememberSnapFlingBehavior(lazyListState = lazyListState)
             ) {
-                items(options) { option ->
+                items(items) { item ->
                     Box(
                         modifier = Modifier
                             .height(pickerItemHeight)
                             .fillMaxWidth(),
                         contentAlignment = Alignment.Center
                     ) {
-                        OText(text = option, style = OTextStyle.Subtitle3)
+                        OText(text = item, style = OTextStyle.Body2)
                     }
                 }
             }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun OPickerPreview() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Column(
-            modifier = Modifier
-                .height(200.dp)
-                .background(Color.White)
-        ) {
-            OPicker(
-                options = (1..10).toList().map { "$it" },
-                selectedIndex = 0,
-                onSelect = {}
-            )
         }
     }
 }
