da8371fc2c0f3c68b0ac87ed4ba02ae494f1ba6d
junyoungleee <jun991203@naver.com>
2025-06-29 01:30:01 +0900

    [refactor] update apiEndPoint file as swagger docs

---
+++ data/src/main/java/com/begin_a_gain/data/remote/constant/ApiEndPoint.kt
@@ -0,0 +1,13 @@
+package com.begin_a_gain.data.remote.constant
+
+sealed class ApiEndPoint(val endPoint: String) {
+
+    data object Auth : ApiEndPoint("auth")
+    fun Auth.kakaoSignIn(): String = this.endPoint + "/login/oauth/kakao"
+    fun Auth.tokenRefresh(): String = this.endPoint + "/token/refresh"
+
+    data object User : ApiEndPoint("users")
+    fun User.info() = this.endPoint + "/info"
+    fun User.updateNickName() = this.endPoint + "/nicknames"
+    fun User.nickNameValidation() = this.endPoint + "/nicknames/validations"
+}
