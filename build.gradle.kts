1b49f334ca03d3465ecb79a57272f422efb882eb
junyoungleee <jun991203@naver.com>
2026-03-30 00:52:06 +0900

    [add] add firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.kotlinx.serialization) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.google.services) apply false
 }
